b8ee36398648ff35f1b0741f482069a3f079f04f
siyabonga <siyabongakhanyile76@gmail.com>
2025-10-31 12:38:37 +0200

    mod:changed log method

--- utils/logger.js
+++ utils/logger.js
@@ -1,36 +1,12 @@
-const path = require("path");
-const fs = require("fs");
-var express = require('express')
 var morgan = require('morgan')
-var rfs = require('rotating-file-stream') // version 2.x
-var app = express()
+var path = require('path')
+var rfs = require('rotating-file-stream')
 
 // create a rotating write stream
 var accessLogStream = rfs.createStream('access.log', {
-  interval: '1d', // rotate daily
-  path: path.join(__dirname, 'log')
+  interval: '1d', 
+  path: path.join(__dirname, '../logger')
 })
 
 
-const logger = (tokens, req, res) => {
-  //do something
-  const logger = [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, "content-length"),
-    "-",
-    tokens["response-time"](req, res),
-    "ms",
-  ].join(" ");
-
-  fs.appendFileSync(
-    path.join(__dirname, "logger", "logger.txt"),
-    `${logger}\n`
-  );
-
-  return morgan(logger,{ stream: accessLogStream });
-};
-
-
-module.exports = logger;
+module.exports = morgan('combined', { stream: accessLogStream })
