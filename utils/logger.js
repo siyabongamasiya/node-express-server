6a96674c00dc1c3410e11720747c5912c0b815bc
siyabonga <siyabongakhanyile76@gmail.com>
2025-10-31 11:58:56 +0200

    mod ; created a logstream and returned morgan

--- utils/logger.js
+++ utils/logger.js
@@ -2,26 +2,24 @@ const path = require("path");
 const fs = require("fs");
 const morgan = require("morgan");
 
+// create a write stream (in append mode)
+const logStream = fs.createWriteStream(
+  path.join(__dirname, "logger", "logger.txt"),
+  { flags: "a" } // 'a' means append
+);
 
-const logger = (tokens, req, res) => {
-  //do something
-  const logger = [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, "content-length"),
-    "-",
-    tokens["response-time"](req, res),
-    "ms",
-  ].join(" ");
-
-  fs.appendFileSync(
-    path.join(__dirname, "logger", "logger.txt"),
-    `${logger}\n`
-  );
-
-  return morgan(logger);
-};
-
+// create a custom morgan format and write to file
+const logger = morgan(
+  (tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"), "-",
+      tokens["response-time"](req, res), "ms"
+    ].join(" ");
+  },
+  { stream: logStream } // tell morgan to write to our file
+);
 
 module.exports = logger;
