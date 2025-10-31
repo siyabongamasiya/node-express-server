113478d693806a10fd95d31c0f6f0f56381bdbd4
siyabonga <siyabongakhanyile76@gmail.com>
2025-10-31 12:21:47 +0200

    mod:added stream object to the morgan function

--- utils/logger.js
+++ utils/logger.js
@@ -1,27 +1,36 @@
-// const path = require("path");
-// const fs = require("fs");
-// const morgan = require("morgan");
+const path = require("path");
+const fs = require("fs");
+var express = require('express')
+var morgan = require('morgan')
+var rfs = require('rotating-file-stream') // version 2.x
+var app = express()
+
+// create a rotating write stream
+var accessLogStream = rfs.createStream('access.log', {
+  interval: '1d', // rotate daily
+  path: path.join(__dirname, 'log')
+})
 
 
-// const logger = (tokens, req, res) => {
-//   //do something
-//   const logger = [
-//     tokens.method(req, res),
-//     tokens.url(req, res),
-//     tokens.status(req, res),
-//     tokens.res(req, res, "content-length"),
-//     "-",
-//     tokens["response-time"](req, res),
-//     "ms",
-//   ].join(" ");
+const logger = (tokens, req, res) => {
+  //do something
+  const logger = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+  ].join(" ");
 
-//   fs.appendFileSync(
-//     path.join(__dirname, "logger", "logger.txt"),
-//     `${logger}\n`
-//   );
+  fs.appendFileSync(
+    path.join(__dirname, "logger", "logger.txt"),
+    `${logger}\n`
+  );
 
-//   return morgan(logger);
-// };
+  return morgan(logger,{ stream: accessLogStream });
+};
 
 
-// module.exports = logger;
+module.exports = logger;
